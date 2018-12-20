233f427f1a58b2e6d947607892a466c229580e5e
moalexey <moalexey@yandex.ru>
2018-12-20 11:29:16 +0300

    Update lists_intersection.py

--- lists_intersection.py
+++ lists_intersection.py
@@ -6,7 +6,7 @@ import re
 
 my_text_1_filename = 'my_text_1.txt'
 my_text_2_filename = 'my_text_2.txt'
-my_result_filename  = 'unique_words.txt'
+my_result_filename  = 'not_unique_words.txt'
 
 text_1_file = open(my_text_1_filename).read().decode('utf8').lower()
 text_2_file = open(my_text_2_filename).read().decode('utf8').lower()
@@ -16,9 +16,9 @@ wordlist_1 = re.compile(r'\W+', re.UNICODE).split(text_1_file)
 wordlist_2 = re.compile(r'\W+', re.UNICODE).split(text_2_file)
 
 # Intersection
-unique_words = set.intersection(*map(set, [filter(None, wordlist_1), filter(None, wordlist_2)]))
+not_unique_words = set.intersection(*map(set, [filter(None, wordlist_1), filter(None, wordlist_2)]))
 
 # Write result in file
 with open(my_result_filename, 'w') as result:
-    for word in unique_words:
+    for word in not_unique_words:
         result.write('{w}\n'.format(w=word.encode('utf8')))
